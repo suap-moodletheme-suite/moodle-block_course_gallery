abb2e3a28b52f46a5adb611edc63a3fe26af066c
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-09-25 19:13:56 -0300

    feat(upgrade): [ADD] Preenche automaticamente categorias em instâncias no upgrade do bloco
    Motivo:
    Após a introdução do filtro por categorias na issue #9, instâncias existentes do bloco sem categorias configuradas deixavam de exibir cursos após a atualização.
    
    Impacto:
    Popula automaticamente as configurações de categorias em instâncias existentes do bloco durante o upgrade do plugin, mantendo a listagem de todos os cursos elegíveis com autoinscrição sem requerer intervenção manual.
    
    Arquivos e Mudanças:
    - db/upgrade.php: criada função xmldb_block_course_gallery_upgrade para preencher a configuração de categorias em instâncias vazias.
    - version.php: incrementada a versão para 2026092508 e release para 0.1.08.
    - tests/upgrade_test.php: criado teste PHPUnit para validar o comportamento da função de upgrade.
    - docs/architecture.rst: atualizado a árvore de arquivos na documentação.

---
+++ tests/upgrade_test.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace block_course_gallery;
+
+/**
+ * PHPUnit tests for block_course_gallery upgrade steps.
+ *
+ * @package    block_course_gallery
+ * @category   test
+ * @copyright  2026 Kelson da Costa Medeiros <kelsoncm@gmail.com>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @covers     \xmldb_block_course_gallery_upgrade
+ */
+final class upgrade_test extends \advanced_testcase {
+    /**
+     * Test upgrade populates empty block instance categories with eligible course categories.
+     */
+    public function test_upgrade_populates_empty_block_instances(): void {
+        global $DB;
+
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+
+        // Create categories and courses with self enrolment.
+        $cat1 = $generator->create_category(['name' => 'Category 1']);
+        $cat2 = $generator->create_category(['name' => 'Category 2']);
+
+        $course1 = $generator->create_course(['category' => $cat1->id, 'fullname' => 'Course 1']);
+        $course2 = $generator->create_course(['category' => $cat2->id, 'fullname' => 'Course 2']);
+
+        $enrolplugin = enrol_get_plugin('self');
+        $enrolplugin->add_instance($course1);
+        $enrolplugin->add_instance($course2);
+
+        // Instance 1: empty configuration.
+        $instance1 = (object)[
+            'blockname' => 'course_gallery',
+            'parentcontextid' => 1,
+            'showinsubcontexts' => 0,
+            'pagetypepattern' => 'site-index',
+            'subpagepattern' => null,
+            'defaultregion' => 'side-pre',
+            'defaultweight' => 0,
+            'configdata' => '',
+            'timecreated' => time(),
+            'timemodified' => time(),
+        ];
+        $instance1->id = $DB->insert_record('block_instances', $instance1);
+
+        // Instance 2: already configured with cat1.
+        $config2 = new \stdClass();
+        $config2->categories = [(int)$cat1->id];
+        $instance2 = (object)[
+            'blockname' => 'course_gallery',
+            'parentcontextid' => 1,
+            'showinsubcontexts' => 0,
+            'pagetypepattern' => 'site-index',
+            'subpagepattern' => null,
+            'defaultregion' => 'side-pre',
+            'defaultweight' => 0,
+            'configdata' => base64_encode(serialize($config2)),
+            'timecreated' => time(),
+            'timemodified' => time(),
+        ];
+        $instance2->id = $DB->insert_record('block_instances', $instance2);
+
+        require_once(__DIR__ . '/../db/upgrade.php');
+
+        $result = xmldb_block_course_gallery_upgrade(2026092407);
+        $this->assertTrue($result);
+
+        // Verify Instance 1 was updated with both cat1 and cat2.
+        $record1 = $DB->get_record('block_instances', ['id' => $instance1->id]);
+        $configdata1 = unserialize(base64_decode($record1->configdata));
+        $this->assertNotEmpty($configdata1->categories);
+        $this->assertContains((int)$cat1->id, $configdata1->categories);
+        $this->assertContains((int)$cat2->id, $configdata1->categories);
+
+        // Verify Instance 2 was not overwritten.
+        $record2 = $DB->get_record('block_instances', ['id' => $instance2->id]);
+        $configdata2 = unserialize(base64_decode($record2->configdata));
+        $this->assertEquals([(int)$cat1->id], $configdata2->categories);
+    }
+}
